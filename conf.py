7580b4a01de10af82eeee29d9d101efad21f9cfa
Libor Jelinek <ljelinek@virtage.com>
2017-12-12 14:44:40 +0100

    conf.py fix

--- conf.py
+++ conf.py
@@ -93,9 +93,6 @@ html_theme = 'basic'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'nosidebar': True,    
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
